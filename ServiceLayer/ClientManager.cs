de88ccd6327ffab0eb1bbec99372176d739e9b83
DKarata <72496940+DKarata@users.noreply.github.com>
2023-06-01 09:51:15 +0300

    Add project files.

---
+++ ServiceLayer/ClientManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+using BusinessLayer;
+
+namespace ServiceLayer
+{
+    class ClientManager
+    {
+        private static ClientContext clientContext;
+        public ClientManager(RestaurantDbContext dbContext)
+        {
+            clientContext = new ClientContext(dbContext);
+        }
+
+        public static void Create(Client client)
+        {
+            try
+            {
+                clientContext.Create(client);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static Client Read(int key)
+        {
+            try
+            {
+                return clientContext.ReadOne(key);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static ICollection<Client> ReadAll()
+        {
+            try
+            {
+                return clientContext.ReadAll();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static void Update(Client client)
+        {
+            try
+            {
+                clientContext.Update(client);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static void Delete(int key)
+        {
+            try
+            {
+                clientContext.Delete(key);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ ServiceLayer/ReservationManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+using BusinessLayer;
+
+namespace ServiceLayer
+{
+    class ReservationManager
+    {
+        private static ReservationContext reservationContext;
+        public ReservationManager(RestaurantDbContext dbContext)
+        {
+            reservationContext = new ReservationContext(dbContext);
+        }
+
+        public static void Create(Reservation reservation)
+        {
+            try
+            {
+                reservationContext.Create(reservation);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static Reservation Read(int key)
+        {
+            try
+            {
+                return reservationContext.ReadOne(key);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static ICollection<Reservation> ReadAll()
+        {
+            try
+            {
+                return reservationContext.ReadAll();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static void Update(Reservation reservation)
+        {
+            try
+            {
+                reservationContext.Update(reservation);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static void Delete(int key)
+        {
+            try
+            {
+                reservationContext.Delete(key);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
